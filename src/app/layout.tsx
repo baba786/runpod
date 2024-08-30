2822f3101325cf3669636fa05fc5c4e2131653a2
Sudhanshu Gautam <sgautam@wikimedia.org>
2024-08-30 20:56:19 +0530

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} flex justify-center items-center min-h-screen`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
